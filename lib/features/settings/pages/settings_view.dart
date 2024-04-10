f4a8c4ad049c6a876900f540f4db4e122b66fc4d
Anthony Ameh <antonineutron@gmail.com>
2024-04-10 15:57:41 +0100

    Nade fetch work

--- lib/features/settings/pages/settings_view.dart
+++ lib/features/settings/pages/settings_view.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class SettingsView extends StatelessWidget {
+class SettingsView extends StatefulWidget {
   const SettingsView({super.key});
 
   @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+/// I used stateful widget because it affords more flexibility
+/// The block of code `setNavigationDelegate(...` helps with keeping track of what happened when the url is loading
+/// I remove the unneccessary callbacks
+/// you can choose to show user custom message incase of error
+/// AND ALSO you can initialize in the initState() callback of the stateless widget
+class _SettingsViewState extends State<SettingsView> {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          print(progress);
+        },
+        onWebResourceError: (WebResourceError error) {
+          print(error.description);
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://google.com'));
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: WebViewWidget(controller: controller),
+    );
   }
 }
